b939399ac1ec0ab636527c08416d335a7315f002
Lencof <71154457+Lencof@users.noreply.github.com>
2021-11-09 18:17:33 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# __Author__ __Lencof__
+# app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+app.run(host='0.0.0.0', port=81)
